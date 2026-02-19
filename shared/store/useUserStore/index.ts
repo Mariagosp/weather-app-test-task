ab322a95c7a26983b15e7c1b95fc81fbf7548093
Maria <m.gosp1704@gmail.com>
2026-02-19 15:38:47 +0200

    fix: logout

--- shared/store/useUserStore/index.ts
+++ shared/store/useUserStore/index.ts
@@ -1,6 +1,9 @@
 import { signOut, User } from 'firebase/auth'
 import { create } from 'zustand'
 import { auth } from '../../../firebase'
+import { removeAuthToken } from '../../../service/authStorage'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { CACHE_KEYS } from '../../const/cache'
 
 interface UseUserStoreType {
     isLoading: boolean
@@ -9,12 +12,14 @@ interface UseUserStoreType {
     logout: () => void
 }
 
-export const useUserStore = create<UseUserStoreType>()((set, get) => ({
+export const useUserStore = create<UseUserStoreType>()((set) => ({
     isLoading: true,
     user: null,
     setUserData: (user) => set({ user, isLoading: false }),
     logout: async () => {
         await signOut(auth)
+        await removeAuthToken()
+        await AsyncStorage.multiRemove([CACHE_KEYS.FAVORITES, CACHE_KEYS.FAVORITES_WEATHER, CACHE_KEYS.HOME_WEATHER])
 
         set({ user: null })
     }
